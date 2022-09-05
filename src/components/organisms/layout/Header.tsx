89d638bea31357ba15ba90f03ed2356abcac4ab1
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-09-05 18:10:45 +0700

    6. ヘッダーの作成(ルーティング機能実装)

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,13 +1,27 @@
 // 4- LOGIN画面のみHeaderを表示しない
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useHistory } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
   // 4- useDisclosure=Drawerの表示を設定するフック
   // - useDisclosureを使うとisOpen, onOpen, onCloseをそれぞれ関数定義しなくても良くなる
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // 6- useHistory() is from react-router-dom
+  const history = useHistory();
+  // 6- use useCallback() to prevent needless rendering
+  const onClickHome = useCallback(() => history.push("/home"), [history]);
+  const onClickUserManagement = useCallback(
+    () => history.push("/home/user_management"),
+    [history]
+  );
+  const onClickSetting = useCallback(() => history.push("/home/setting"), [
+    history
+  ]);
+
   return (
     <>
       {/* 4- as=レンダリングのタグは"nav" */}
@@ -21,7 +35,13 @@ export const Header: VFC = memo(() => {
         padding={{ base: 3, md: 5 }}
       >
         {/* 4- mr = margin-right */}
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursor: "pointer" }}
+          onClick={onClickHome}
+        >
           {/* 4- Headingタグの中にアプリ名を表記 
       - base, md = chakra-uiのbreakpoints
       - baseサイズ画面の場合、fontSizeは"md"　
@@ -40,13 +60,19 @@ export const Header: VFC = memo(() => {
         >
           {/* 4- pr=padding-right */}
           <Box pr={4}>
-            <Link>Users</Link>
+            <Link onClick={onClickUserManagement}>Users</Link>
           </Box>
-          <Link>Settings</Link>
+          <Link onClick={onClickSetting}>Settings</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 });
