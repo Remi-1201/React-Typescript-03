3396bcb155b9032de29fc63714a73a96ef82c8e1
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-09-06 18:24:09 +0700

    7. ログイン画面の作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,31 @@
 // 関数コンポネントなので VFC で型定義, memoで不必要な再レンダリングを防ぐ
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 // propsはないので空にしておく
 export const Login: VFC = memo(() => {
-  return <p>LOGIN PAGE</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          User Control Application
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="User ID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            Login
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
