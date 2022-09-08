c1e33a76b7aab5882213669431abc83fe7fd2d37
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-09-08 20:53:22 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,19 @@
 // 関数コンポネントなので VFC で型定義, memoで不必要な再レンダリングを防ぐ
+import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 // propsはないので空にしておく
 export const UserManagement: VFC = memo(() => {
-  return <p>USER MANAGEMENT PAGE</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl={"https://source.unsplash.com/random"}
+          userName={"Harry"}
+          fullName={"Harry Potter"}
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
