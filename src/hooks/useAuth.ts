2cc6aa13040cbcf115607ea9d32787e08f225575
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-09-08 18:29:11 +0900

    10. メッセージ表示機能の実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,13 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { User } from "../types/api/user";
 import { useHistory } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+
+  const { showMessage } = useMessage();
+
   // useCallback = prevent needless rendering
   // Use id to login
   const [loading, setLoading] = useState(false);
@@ -17,17 +21,29 @@ export const useAuth = () => {
       axios
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
+          // when logged in successfully
           if (res.data) {
+            showMessage({
+              title: "You have successfully logged in",
+              status: "success"
+            });
             history.push("/home");
+            // when failed
           } else {
+            showMessage({
+              title: "Couldn't find that userId",
+              status: "error"
+            });
             alert("Wrong UserId!");
           }
         })
-        .catch(() => alert("The login attempt failed"))
+        .catch(() =>
+          showMessage({ title: "Couldn't find that userId", status: "error" })
+        )
         // to enable finally, write "es2018" in tsconfig.json
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
   // login, loading = useAuth's props
   return { login, loading };
